be20786fb945178ce3c6ac1b9b84d20761c30405
RuiSlayer <79100604+RuiSlayer@users.noreply.github.com>
2023-06-09 06:37:20 +0100

    funcional snail sort
    Fully functional snail sort algorithm with validation for null matrix argument and validation for non squared matrix

--- src/main/java/com/premiumminds/internship/snail/SnailShellPattern.java
+++ src/main/java/com/premiumminds/internship/snail/SnailShellPattern.java
@@ -1,6 +1,9 @@
 package com.premiumminds.internship.snail;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+
 
 /**
  * Created by aamado on 05-05-2023.
@@ -13,7 +16,61 @@ class SnailShellPattern implements ISnailShellPattern {
    * @param matrix matrix of numbers to go through
    * @return order array of values thar represent a snail shell pattern
    */
-  public Future<int[]> getSnailShell(int[][] matrix) {
-    throw new RuntimeException("Not Implemented Yet");
+	
+	
+
+	public Future<int[]> getSnailShell(int[][] matrix) {
+		ExecutorService executor = Executors.newSingleThreadExecutor();
+		
+		Future<int[]> result = executor.submit(() -> {
+		  
+			if(matrix == null || matrix.length == 0) {
+				int [] sortedArray = new int[0];
+				return sortedArray;
+			}
+			
+			if(matrix.length != matrix[0].length) {
+				throw new IllegalArgumentException("A matriz deve ser uma matriz quadrada!!!");
+			}
+
+		  final int arrayLength = matrix.length*matrix.length;
+		  int [] sortedArray = new int[arrayLength];
+		  int top = 0;
+		  int left = 0;
+		  int down = matrix.length-1;
+		  int rigth = matrix.length-1;
+		  int r = 0;
+
+		  while(r < arrayLength) {
+			  //varre a matriz no topo da esquerda para a direita
+			  for(int i = 0; i < rigth-top; i++) {
+				  sortedArray[r] = matrix[left][left + i];
+				  r++;
+			  }
+			  //varre a matriz no lado direito topo de cima para a baixo
+			  for(int i = 0; i < down-top; i++) {
+				  sortedArray[r] = matrix[top + i][down];
+				  r++;
+			  }
+			  top++;
+			  //varre a matriz na base da direita para a esquerda		 
+			  for(int i = rigth; i >= left; i--) {
+				  sortedArray[r] = matrix[rigth][i];
+				  r++;
+			  }
+			  down--;
+			  rigth--;
+			  //varre a matriz na base no lado esquerdo de baixo para cima
+			  for(int i = rigth; i >= top; i--) {
+				  sortedArray[r] = matrix[i][left];
+				  r++;
+			  }
+			  left++;
+		  }
+		  return sortedArray;
+	  });
+
+	  executor.shutdown();
+	  return result;
   };
 }
