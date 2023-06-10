97b9d3273b9b4b548cefee13ce2b1d2eca663c4f
RuiSlayer <79100604+RuiSlayer@users.noreply.github.com>
2023-06-10 08:29:27 +0100

    A few optimisations

--- src/main/java/com/premiumminds/internship/snail/SnailShellPattern.java
+++ src/main/java/com/premiumminds/internship/snail/SnailShellPattern.java
@@ -4,64 +4,59 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-
-/**
- * Created by aamado on 05-05-2023.
- */
 class SnailShellPattern implements ISnailShellPattern {
-
-  /**
-   * Method to get snailshell pattern
-   * 
-   * @param matrix matrix of numbers to go through
-   * @return order array of values thar represent a snail shell pattern
+   /* @param matrix matrix of numbers to go through
+   * @return order array of values that represent a snail shell pattern
    */
-	
-	
 
 	public Future<int[]> getSnailShell(int[][] matrix) {
 		ExecutorService executor = Executors.newSingleThreadExecutor();
 		
 		Future<int[]> result = executor.submit(() -> {
-		  
-			if(matrix == null || matrix.length == 0) {
+			int length = matrix[0].length;
+
+			if(matrix == null || length == 0) {
 				int [] sortedArray = new int[0];
 				return sortedArray;
 			}
 			
-			if(matrix.length != matrix[0].length) {
+			if(matrix.length != length) {
 				throw new IllegalArgumentException("A matriz deve ser uma matriz quadrada!!!");
 			}
+			
+			if(length == 1) {
+				int [] sortedArray = new int[1];
+				sortedArray[0] = matrix[0][0];
+				return sortedArray;
+			}
 
-		  final int arrayLength = matrix.length*matrix.length;
+		  final int arrayLength = length*length;
 		  int [] sortedArray = new int[arrayLength];
 		  int top = 0;
 		  int left = 0;
-		  int down = matrix.length-1;
-		  int rigth = matrix.length-1;
+		  int rigth = length-1;
 		  int r = 0;
-
+		  int i;
 		  while(r < arrayLength) {
 			  //varre a matriz no topo da esquerda para a direita
-			  for(int i = 0; i < rigth-top; i++) {
+			  for( i = 0; i < rigth-top; i++) {
 				  sortedArray[r] = matrix[left][left + i];
 				  r++;
 			  }
 			  //varre a matriz no lado direito topo de cima para a baixo
-			  for(int i = 0; i < down-top; i++) {
-				  sortedArray[r] = matrix[top + i][down];
+			  for( i = 0; i < rigth-top; i++) {
+				  sortedArray[r] = matrix[top + i][rigth];
 				  r++;
 			  }
 			  top++;
 			  //varre a matriz na base da direita para a esquerda		 
-			  for(int i = rigth; i >= left; i--) {
+			  for( i = rigth; i >= left; i--) {
 				  sortedArray[r] = matrix[rigth][i];
 				  r++;
 			  }
-			  down--;
 			  rigth--;
 			  //varre a matriz na base no lado esquerdo de baixo para cima
-			  for(int i = rigth; i >= top; i--) {
+			  for( i = rigth; i >= top; i--) {
 				  sortedArray[r] = matrix[i][left];
 				  r++;
 			  }
